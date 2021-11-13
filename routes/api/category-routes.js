e08ed1e62da6167ac92248eafc1c90a333a0c0c1
Dave Quinn <dquinn8@cogeco.ca>
2021-11-13 12:38:54 -0500

    added POST and PUT routes to category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
                 attributes: ['product_name']
             }]
         })
-        .then(dbPostData => res.json(dbPostData))
+        .then(dbData => res.json(dbData))
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
@@ -35,7 +35,7 @@ router.get('/:id', (req, res) => {
                 attributes: ['product_name']
             }]
         })
-        .then(dbPostData => res.json(dbPostData))
+        .then(dbData => res.json(dbData))
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
@@ -44,10 +44,30 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
     // create a new category
+    Category.create({
+            category_name: req.body.category_name
+        })
+        .then(dbData => res.json(dbData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
 });
 
 router.put('/:id', (req, res) => {
     // update a category by its `id` value
+    Category.update({
+            category_name: req.body.category_name
+        }, {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(dbData => res.json(dbData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
 });
 
 router.delete('/:id', (req, res) => {
