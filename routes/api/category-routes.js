cc4a7d724502ef6d2c8f24aa79e8016b7c523667
Dave Quinn <dquinn8@cogeco.ca>
2021-11-13 12:31:09 -0500

    added routs for GETing all and specific categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,25 +4,54 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+    // find all categories
+    // be sure to include its associated Products
+
+    Category.findAll({
+            // Query configuration
+            include: [{
+                model: Product,
+                attributes: ['product_name']
+            }]
+        })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+    // find one category by its `id` value
+    // be sure to include its associated Products
+
+    Category.findAll({
+            // Query configuration
+            where: {
+                id: req.params.id
+            },
+            include: [{
+                model: Product,
+                attributes: ['product_name']
+            }]
+        })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+    // create a new category
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+    // update a category by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+    // delete a category by its `id` value
 });
 
 module.exports = router;
