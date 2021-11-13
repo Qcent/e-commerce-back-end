b1f26db2ba19ceabe808f3c2ed93740e03b65808
Dave Quinn <dquinn8@cogeco.ca>
2021-11-13 12:40:21 -0500

    added DELETE  route to category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -72,6 +72,16 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
     // delete a category by its `id` value
+    Category.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(dbData => res.json(dbData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
 });
 
 module.exports = router;
