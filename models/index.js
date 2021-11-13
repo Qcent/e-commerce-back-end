104471343516ad8891cc8c8a618debb407905ebf
Dave Quinn <dquinn8@cogeco.ca>
2021-11-13 12:20:42 -0500

    created required model associations in models/index.js

--- models/index.js
+++ models/index.js
@@ -5,16 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product)
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+    through: ProductTag,
+    as: 'Associated_Tags'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+    through: ProductTag,
+    as: 'Associated_Products'
+});
 
 module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
+    Product,
+    Category,
+    Tag,
+    ProductTag,
 };
