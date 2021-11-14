c7729c07060e684b98f13f92eb1f7b70b634c006
Dave Quinn <dquinn8@cogeco.ca>
2021-11-14 09:52:36 -0500

    commented on URI string generation

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+// Create URI string from environment variables defined in .env file
 const DB_URI = `mysql://${process.env.DB_USER}:${process.env.DB_PW}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
 const sequelize = process.env.JAWSDB_URL ?
